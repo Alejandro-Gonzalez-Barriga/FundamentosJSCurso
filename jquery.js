9483056f5f087c16600bea62731c5ba240b10849
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-12-15 15:11:04 -0600

    added callbacks

--- jquery.js
+++ jquery.js
@@ -1,11 +1,29 @@
 const API_URL = "https://swapi.co/api/";
 const PEOPLE_URL = "people/:id";
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const options = { crossDomanin: true };
-const cb = function(character) {
-  console.log(arguments);
+
+const getPeople = function(character) {
+  //console.log(arguments);
+  //console.log(`${API_URL}people`);
   console.log(character.name);
 };
 
-$.get(lukeUrl, options, cb);
+function getCharacter(id, cb) {
+  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+  $.get(url, options, getPeople);
+  if (cb) {
+    cb();
+  }
+}
+
+//callback hell
+
+getCharacter(1, function() {
+  getCharacter(2, function() {
+    getCharacter(3, function() {
+      getCharacter(4, function() {
+        getCharacter(5);
+      });
+    });
+  });
+});
