eff313df9d1d15a68283083b3358c8548de54c29
alexG <alejandro.gonzalez.barriga@gmail.com>
2019-12-15 16:28:45 -0600

    added promises

--- jquery.js
+++ jquery.js
@@ -8,13 +8,48 @@ const getPeople = function(character) {
   console.log(character.name);
 };
 
-function getCharacter(id, cb) {
-  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
-  $.get(url, options, getPeople);
-  if (cb) {
-    cb();
-  }
+function getCharacter(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+    $.get(url, options, function(data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
 }
+
+function onError(id) {
+  console.log(`error with ${id}`);
+}
+
+getCharacter(1)
+  .then(character => {
+    console.log(`1 is ${character.name}`);
+    return getCharacter(2);
+  })
+  .then(character => {
+    console.log(`2 is ${character.name}`);
+    return getCharacter(3);
+  })
+  .then(character => {
+    console.log(`3 is ${character.name}`);
+    return getCharacter(4);
+  })
+  .then(character => {
+    console.log(`4 is ${character.name}`);
+    return getCharacter(5);
+  })
+  .then(character => {
+    console.log(`5 is ${character.name}`);
+    return getCharacter(6);
+  })
+  .then(character => {
+    console.log(`6 is ${character.name}`);
+    return getCharacter(7);
+  })
+  .then(character => {
+    console.log(`7 is ${character.name}`);
+  })
+  .catch(onError);
 
 //callback hell
 
